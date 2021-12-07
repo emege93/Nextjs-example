67a8e6492a8e55b2ded02f16b4ecdca52e694d42
emege93 <emege93@gmail.com>
2021-12-07 17:59:31 +0100

    add page paths

--- pages/blog/index.jsx
+++ pages/blog/index.jsx
@@ -1,9 +1,36 @@
+import Link from "next/link";
 import Layout from "../../components/layout";
 
-export default function Index() {
+export default function Index({ data }) {
   return (
     <Layout>
       <h1>Lista de posts</h1>
+      {data.map(({ id, title, body }) => (
+        <div key={id}>
+          <h3>
+            <Link href={`/blog/${id}`}>
+              <a>
+                {id} - {title}
+              </a>
+            </Link>
+          </h3>
+          <p>{body}</p>
+        </div>
+      ))}
     </Layout>
   );
+}
+
+export async function getStaticProps() {
+  try {
+    const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+    const data = await res.json();
+    return {
+      props: {
+        data,
+      },
+    };
+  } catch (error) {
+    console.error(error);
+  }
 }
