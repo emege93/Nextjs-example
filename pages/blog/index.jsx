a37280bda493c2109abc7047d7dc88425c3e02ff
emege93 <emege93@gmail.com>
2021-12-07 16:54:18 +0100

    add pages and styles

---
+++ pages/blog/index.jsx
@@ -0,0 +1,9 @@
+import Layout from "../../components/layout";
+
+export default function Index() {
+  return (
+    <Layout>
+      <h1>Lista de posts</h1>
+    </Layout>
+  );
+}
---
+++ pages/contact.jsx
@@ -0,0 +1,9 @@
+import Layout from "../components/layout";
+
+export default function Contact() {
+  return (
+    <Layout title="Contact | Next.js" description="Add description">
+      <h1>Contact</h1>
+    </Layout>
+  );
+}
